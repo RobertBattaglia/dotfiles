8837edecd3794b55553de38ee39e08d1c79848ac
Robert Battaglia <r083rt164@gmail.com>
2023-04-20 17:13:37 -0400

    add basic lsp

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -11,18 +11,39 @@ return require('packer').startup(function(use)
 	-- Theme
 	use({ 'rose-pine/neovim', as = 'rose-pine' })
 
-	-- Buffers
-	use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
-
 	-- Parser
 	use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 	use 'nvim-treesitter/playground'
 
-	-- Undo History
+	-- Visual Undo stack as tree
 	use 'mbbill/undotree'
 
-	-- Git features
+	-- Git Superpowers
 	use 'tpope/vim-fugitive'
+
+	-- lsp
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v1.x',
+		requires = {
+			-- LSP Support
+			'neovim/nvim-lspconfig',  -- Required
+			'williamboman/mason.nvim',
+			'williamboman/mason-lspconfig.nvim',
+
+		-- Autocompletion
+		{'hrsh7th/nvim-cmp'},     -- Required
+		{'hrsh7th/cmp-nvim-lsp'}, -- Required
+		{'L3MON4D3/LuaSnip'},     -- Required
+	};
+
+
+	-- Buffers
+	use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'},
+
+	-- Status Bar
+	use {'nvim-lualine/lualine.nvim', requires = { 'nvim-tree/nvim-web-devicons', opt = true}},
+}
 
 end)
 
